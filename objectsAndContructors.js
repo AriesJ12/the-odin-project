acf178bb76d630b1d0b10a115e60e5f90d63fa8b
AriesJ <ajtagle12@gmail.com>
2025-07-30 18:35:03 +0800

    done exercise

---
+++ objectsAndContructors.js
@@ -0,0 +1,18 @@
+function Book(title, author, pages, hasRead)
+{
+    if (!new.target){
+        throw Error("You must use the word 'new' operator to call the constructor")
+    }
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.hasRead = hasRead
+    this.info = function() {
+        let readText = "has not read yet"
+        if (this.hasRead) {
+            readText = "has read"
+        }
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readText}`
+    }
+}
+
